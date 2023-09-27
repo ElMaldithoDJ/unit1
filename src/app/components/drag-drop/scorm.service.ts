615ba4e3e8326b20141f73a8c5bba7225d231144
BS <isaacbladehd@gmail.com>
2023-09-27 10:39:19 -0400

    stop scorm comunication with angular

--- src/app/components/drag-drop/scorm.service.ts
+++ src/app/components/drag-drop/scorm.service.ts
@@ -1,19 +1,23 @@
-import { Injectable } from "@angular/core";
-import { ScormApiWrapper } from "./util/scorm";
+import { Injectable } from '@angular/core';
+import { ScormApiWrapper } from './util/scorm';
 
 @Injectable()
 export class ScormService {
-
-  constructor(
-    private scorm: ScormApiWrapper
-  ) {
-    this.scorm.initialize();
-    this.scorm.dataSet('cmi.core.score.min', '0');
-    this.scorm.dataSet('cmi.core.score.max', '5');
+  constructor(private scorm: ScormApiWrapper) {
+    if (this.scorm.initialize()) {
+      this.scorm.initialize();
+      this.scorm.dataSet('cmi.core.score.min', '0');
+      this.scorm.dataSet('cmi.core.score.max', '5');
+    }
   }
 
-  setScore(data: string): void {
-    this.scorm.dataSet('cmi.core.score.raw', data);
+  setScore(data: number): void {
+    this.scorm.dataSet('cmi.core.score.raw', `${data}`);
   }
 
+  stopScorm(): void {
+    if(!this.scorm.terminate()) {
+      this.scorm.terminate();
+    }
+  }
 }
