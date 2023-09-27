70f7516f83fb0d49c1e95f30d35d17f4857d16ef
BS <isaacbladehd@gmail.com>
2023-09-27 10:29:06 -0400

    minimal changes

--- src/app/components/drag-drop/drag-drop.component.ts
+++ src/app/components/drag-drop/drag-drop.component.ts
@@ -42,8 +42,7 @@ import {
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DragDropComponent
-  implements ControlValueAccessor, OnInit, AfterViewInit, OnDestroy
+export class DragDropComponent implements OnInit, AfterViewInit, OnDestroy
 {
   @Input() value: any;
   @Output() valueChange: EventEmitter<any> = new EventEmitter();
@@ -99,9 +98,6 @@ export class DragDropComponent
   aunswersOpts: string[] = [];
   aunswers: string[] = [];
 
-  onChangeCb: Function = (value?: any) => void 0;
-  onTouchCb: Function = () => void 0;
-
   constructor(private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
@@ -109,36 +105,6 @@ export class DragDropComponent
   }
 
   ngAfterViewInit(): void {}
-
-  writeValue(obj: any): void {
-    this.value = obj;
-    this.valueChange.emit(this.value);
-  }
-  registerOnChange(fn: any): void {
-    this.onChangeCb = fn;
-  }
-  registerOnTouched(fn: any): void {
-    this.onTouchCb = fn;
-  }
-  setDisabledState?(isDisabled: boolean): void {
-    this.disabled = isDisabled;
-    this.cdr.markForCheck();
-  }
-
-  setAunswer(index: number, aunswer: string): void {
-    this.cuestions[index].aunswer = aunswer;
-    this.cdr.markForCheck();
-    this.onChangeCb(this.value);
-  }
-
-  sendValue(): void {
-    const correct: number =
-    this.cuestions.filter((item) => item.correct === true).length * 0.5;
-    this.value = parseInt(`${correct}`, 10);
-    this.valueChange.emit(this.value);
-    this.onChangeCb(this.value);
-    this.cdr.markForCheck();
-  }
 
   onDrag(event: DragEvent): void {
     event.preventDefault();
@@ -168,24 +134,18 @@ export class DragDropComponent
   }
 
   onDrop(event: CdkDragDrop<string[]>): void {
-    if(event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        this.aunswers,
-        event.previousIndex,
-        event.currentIndex
-      );
+      const id = this.aunswersOpts.findIndex(a => a === event.item.data);
+      this.aunswers = [...this.aunswers, event.item.data];
+      this.aunswersOpts.splice(id, 1);
+      this.cdr.markForCheck();
     }
   }
 
   setAunswersOpts(): void {
-    const cuestions = this.cuestions.map(val => val.correct_aunswer).slice();
+    const cuestions = this.cuestions.slice().map(x => x.correct_aunswer);
     this.aunswersOpts = shuffleItems(cuestions);
     this.cdr.markForCheck();
   }
@@ -197,8 +157,8 @@ export class DragDropComponent
       c = [...c, (this.cuestions[x].correct_aunswer === this.aunswers[x])];
     }
     const cx = (c.filter(i => i === true).length * .5);
-    this.cdr.markForCheck();
     console.log(cx);
+    this.cdr.markForCheck();
   }
 
   ngOnDestroy(): void {
