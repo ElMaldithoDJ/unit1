908e701288c5c584df49ff6c5d5795e4ad9b7f24
BS <isaacbladehd@gmail.com>
2023-09-27 10:28:59 -0400

    Shuffle the aunsewers

--- src/app/components/drag-drop/util/shuffle.ts
+++ src/app/components/drag-drop/util/shuffle.ts
@@ -1,5 +1,5 @@
-export const shuffleItems = <ICuestion>(array: ICuestion[]): ICuestion[] => {
-  let x:ICuestion[] = array;
+export function shuffleItems(array: string[]): string[] {
+  let x:string[] = array;
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
